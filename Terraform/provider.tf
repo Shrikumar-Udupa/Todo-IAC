eefb4b7edc8017480bf4770f6d6aba0e3d031f1f
Shrikumar-Udupa <shrikumarudupa99@gmail.com>
2024-09-09 19:49:51 +0530

    provider added

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -5,18 +5,23 @@ terraform {
       version = "5.65.0"
     }
   }
+  backend "s3" {
+  bucket         	   = "g628t-todo-s3-tfstate"
+  key              	   = "state/terraform.tfstate"
+  region         	   = "us-east-1"
+  encrypt        	   = true
+  dynamodb_table = "g628t-todo-tfstate-dynamodb"
+  }
 }
 
 provider "aws" {
   assume_role {
-    role_arn     = "arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME"
-    session_name = "SESSION_NAME"
-    external_id  = "EXTERNAL_ID"
+    role_arn     = "arn:aws:iam::825765396418:role/g628t-todo-terraform-role" 
+    session_name = "g628t-todo-terraform"
   }
   region = "us-east-1"
 }
 
-# Route table
 
 
 
